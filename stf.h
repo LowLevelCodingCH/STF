2bfd723f530e1751b70917e92ace7694f04a5c16
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2024-05-04 18:29:02 +0300

    Add files via upload

--- stf.h
+++ stf.h
@@ -3,10 +3,70 @@
 
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 #define coint const int
 
 namespace stf {
+    int convint(char c){
+        if(c == '0'){
+            return 0;
+        }
+        else if(c == '1'){
+            return 1;
+        }
+        else if(c == '2'){
+            return 2;
+        }
+        else if(c == '3'){
+            return 3;
+        }
+        else if(c == '4'){
+            return 4;
+        }
+        else if(c == '5'){
+            return 5;
+        }
+        else if(c == '6'){
+            return 6;
+        }
+        else if(c == '7'){
+            return 7;
+        }
+        else if(c == '8'){
+            return 8;
+        }
+        else if(c == '9'){
+            return 9;
+        }
+        else{
+            return 9999;
+        }
+    }
+    bool checkd(char c){
+        if((int)c >= 48 && (int)c <= 57){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    std::vector<std::string> split(std::string string, char delimiter){
+        std::string buffer;
+        std::vector<std::string> result;
+        for(char c : string){
+            if(c == delimiter){
+                result.push_back(buffer);
+                buffer = "";
+            }
+            else{
+                buffer.push_back(c);
+            }
+        }
+        result.push_back(buffer);
+        return result;
+    }
+
     template<int C>
     class string {
     public:
